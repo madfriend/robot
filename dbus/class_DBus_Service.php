a6754222adc7600caee3c92cda58e1bf6c5684bb
madfriend <scarywound@gmail.com>
2012-08-29 21:56:22 +0400

    dbus is optional

--- dbus/class_DBus_Service.php
+++ dbus/class_DBus_Service.php
@@ -7,6 +7,12 @@ namespace Bot {
       static $dbus = NULL;
 
       static public function register($name, $interface, $class = __CLASS__) {
+         if (!class_exists('\DBus')) {
+           trigger_error('You don\t have php-dbus extension \
+                          installed. If you don\'t want to use \
+                          bot as a dbus service, comment out bot::register call.');
+           die();
+         }
          $dbus =  new \DBus(\DBus::BUS_SESSION, TRUE);
          $dbus->requestName($name);
          $dbus->registerObject($interface, $name, $class);
